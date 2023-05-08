fb46e1c9048ce1d5d22fa6a0299fe6dddf87097d
Dr. Peter Dillinger <peter.dillinger@srh.de>
2023-05-08 15:45:46 +0200

    Unsinn mit for-Schleifen...

--- src/Primzahlen.java
+++ src/Primzahlen.java
@@ -6,6 +6,20 @@ public class Primzahlen {
         System.out.print("Bitte geben Sie eine Zahl ein: ");
         int zahl = eingabe.nextInt();
 
+        byte b = 127;
+        System.out.println("Mein Byte: " + b);
+        b++;
+        System.out.println("Mein Byte: " + b);
+
+        // typische Aufbau von for-Schleifen
+        for (int i=0, j=7, k=-15; i < 10 && j > 0 && k < 10; i++, j+=2, k-=3) {
+            System.out.println("Mein Index: " + i + ", " + j + ", " + k);
+        }
+
+        for (byte i=10; i > 0; ++i) {
+            System.out.println("Der andere Index: " + i);
+        }
+
         // Verarbeitung
         int teiler;
         for ( teiler = 2 ; teiler < zahl ; teiler++ ) {
