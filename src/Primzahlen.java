9354a8b60675f8d1478341e598bd1b592eeff07f
Dr. Peter Dillinger <peter.dillinger@srh.de>
2023-05-08 16:02:44 +0200

    Primzahlgenerator, d.h. Primzahlenfinder, implementiert mit Verwendung einer Methode!

--- src/Primzahlen.java
+++ src/Primzahlen.java
@@ -1,35 +1,30 @@
 import java.util.Scanner;
 
 public class Primzahlen {
+
     public static void main(String[] args) {
         Scanner eingabe = new Scanner(System.in);
-        System.out.print("Bitte geben Sie eine Zahl ein: ");
-        int zahl = eingabe.nextInt();
+        System.out.print("Bitte geben Sie die Startzahl ein: ");
+        int startzahl = eingabe.nextInt();
+        System.out.print("Bitte geben Sie die Anzahl ein: ");
+        int anzahl = eingabe.nextInt();
 
-        byte b = 127;
-        System.out.println("Mein Byte: " + b);
-        b++;
-        System.out.println("Mein Byte: " + b);
-
-        // typische Aufbau von for-Schleifen
-        for (int i=0, j=7, k=-15; i < 10 && j > 0 && k < 10; i++, j+=2, k-=3) {
-            System.out.println("Mein Index: " + i + ", " + j + ", " + k);
+        while (anzahl > 0) {
+            int zahl = startzahl;
+            if ( istPrimzahl(zahl) ) {
+                System.out.println(zahl);
+                anzahl--;
+            }
+            startzahl++;
         }
+    }
 
-        for (byte i=10; i > 0; ++i) {
-            System.out.println("Der andere Index: " + i);
-        }
-
-        // Verarbeitung
-        int teiler;
-        for ( teiler = 2 ; teiler < zahl ; teiler++ ) {
-            if ( zahl % teiler == 0 ) {
-                System.out.println("Die Zahl ist keine Primzahl.");
-                System.out.println("Folgender Teiler wurde gefunden: " + teiler);
-                break;
+    private static boolean istPrimzahl(int zahl) {
+        for (int teiler = 2; teiler < zahl; teiler++) {
+            if (zahl % teiler == 0) {
+                return false;
             }
         }
-        if ( teiler == zahl )
-            System.out.println("Die Zahl ist eine Primzahl");
+        return true;
     }
 }
